581af52e3d15fa30fffc4c6ba856ca31d559e56a
Vincent Boon <vincent@justbetter.nl>
2023-05-25 11:53:41 +0200

    Make job unique

--- src/Jobs/CheckAllKnownProductsExistenceJob.php
+++ src/Jobs/CheckAllKnownProductsExistenceJob.php
@@ -3,13 +3,14 @@
 namespace JustBetter\MagentoProducts\Jobs;
 
 use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use JustBetter\MagentoProducts\Contracts\ChecksAllKnownProducts;
 
-class CheckAllKnownProductsExistenceJob implements ShouldQueue
+class CheckAllKnownProductsExistenceJob implements ShouldQueue, ShouldBeUnique
 {
     use Dispatchable;
     use InteractsWithQueue;
